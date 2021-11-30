932265901a9e8357a5d8e632c1bda1564cb529c1
DELL <dalijlasii@gmail.com>
2021-11-30 21:37:46 +0100

    TodosScreen

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,42 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  class TodosScreen extends StatelessWidget {
+  // Requiring the list of todos.
+  const TodosScreen({Key? key, required this.todos}) : super(key: key);
 
+  final List<Todo> todos;
+
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+  appBar: AppBar(
+  title: const Text('Todos'),
+  ),
+  //passing in the ListView.builder
+  body: ListView.builder(
+  itemCount: todos.length,
+  itemBuilder: (context, index) {
+  return ListTile(
+  title: Text(todos[index].title),
+  // When a user taps the ListTile, navigate to the DetailScreen.
+  // Notice that you're not only creating a DetailScreen, you're
+  // also passing the current todo through to it.
+  onTap: () {
+  Navigator.push(
+  context,
+  MaterialPageRoute(
+  builder: (context) => DetailScreen(todo: todos[index]),
+  ),
+  );
+  },
+  );
+  },
+  ),
+
+  );
+  }
+  }
 
 
   class DetailScreen extends StatelessWidget {
